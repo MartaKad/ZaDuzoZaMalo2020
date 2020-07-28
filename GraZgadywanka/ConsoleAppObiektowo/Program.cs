283d754db45565f5bc9f1f06a01a420cc87aa292
MartaKad <kadluczka@gmail.com>
2020-07-28 19:52:54 +0200

    ver.obiektowo

---
+++ GraZgadywanka/ConsoleAppObiektowo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using GraLibrary;
+
+namespace ConsoleAppObiektowo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
